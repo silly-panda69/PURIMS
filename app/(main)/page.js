52c29dcde2d3e1abb463761fa3dceac889865358
pankaj114425 <115251359+pankaj114425@users.noreply.github.com>
2024-04-29 14:03:10 +0530

    main_page

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -35,13 +35,31 @@ export default async function Home() {
 	data = data?.filter((d) => d._id != "pu");
 	const subjectChart = await getDepartmentSubjectChart("pu");
 	const yearlyChart = await getDepartmentYearlyChart("pu");
-	const pubChart = await getDepartmentPubChart("pu");
+	const  pubChartData = await getDepartmentPubChart("pu");
+
+   const pubChart = await Promise.all(
+    pubChartData.map(async (item) => {
+      return {
+        _id: item._id,
+        id: item._id.sourceID,
+        label: item._id.source,
+
+        metrics: item.metrics,
+        value: item.value,
+      };
+    })
+  );
+
+
+
+	
 	const subtypeChart = await getDepartmentSubtypeChart("pu");
 	const world = await getDepartmentWorldChart("pu");
 	const metrics = await getMetrics({ dept: "pu" });
 	const totalProjectFund= await univprojectfund();
 	const totalPhds=await univphds();
 	const totalProjects=await univprojects();
+	
 	const { TWEET_COUNT} = await getDepartmentSocialMetrics("pu");
 	{ let impact = pubChart.reduce(
 		(p, t) => p + (parseInt(t.metrics?.impactFactorData?.metrics?.impactMetrics?.jif) || 0) * t.value,
